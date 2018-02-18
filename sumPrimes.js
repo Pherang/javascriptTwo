f4b9ae3a7d1e7b3b4003752e41ef81141f434636
Pherang <Pherang@users.noreply.github.com>
2018-02-18 09:50:10 -0500

    Update sumPrimes.js
    Changed formula to test for primes.

--- sumPrimes.js
+++ sumPrimes.js
@@ -11,9 +11,20 @@ function sumPrimes(num) {
    return acc+curr;
   }
 
-  for (i=2; i < num; i++ ) {
-
-      //Conditions to not push
+  for (i=2; i < num; i++ ) {for (i=4; i < num; i++ ) {
+    
+    // All prime numbers can be found by testing 6*k +
+    // Test i to see if it is divisible by any a = 6k + 1 <= sqrt()
+    // 6K +- 1 <= squareroot of Math.round(Math.sqrt(i))
+    
+    
+      for (j=1; 6*j + 1 <= Math.round(Math.sqrt(i)); j++) {
+    
+      console.log("Testing prime " + 6*j-1);
+      console.log("Testing prime " + 6*j+1);
+      // Otherwise it's probably prime.
+    }
+      primeNums.push(i);
 
 
 
