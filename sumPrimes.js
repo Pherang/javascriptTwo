8aaee46e0d3fe7f772955d89d691bb5ca7850590
unknown <Pherang@users.noreply.github.com>
2018-02-19 11:34:15 -0500

    Algorith finally works in checking primes. It definitely needs optiziming.

--- sumPrimes.js
+++ sumPrimes.js
@@ -6,51 +6,53 @@ function sumPrimes(num) {
   var primeHigh;
   var primeLow;
   var primeNums = [];
-  var notPrime = true;
+  var notPrime = false;
   function reducer (acc, curr) {
    return acc+curr;
   }
-    
+
   if (num > 3) {
+
     primeNums.push(2);
     primeNums.push(3);
       for (i = 4; i <= num; i++) {
-        
+        notPrime = false;     // Reset this flag here instead of at the end. Loop kept not adding 5 as a prime number since it's the first prime after 3.
         console.log("Testing " + i);
         if (i % 2 == 0 || i % 3 == 0 ){
           console.log("Divisible by two or three");
-          
+
         } else {
             console.log("Made it to check primes");
             for (k=1; (6*k-1) <= i; k++ ) {
               primeHigh = (6*k)-1;
               primeLow = (6*k)+1;
               console.log("Testing " + i + " against " + primeHigh + " " + primeLow);
-            
-             
+
+
               if ( (i != primeHigh && i != primeLow) && (i % primeHigh == 0 || i % primeLow == 0)) {
                 console.log("Not a prime " + i + " mod " + primeHigh + " is " + (i % primeHigh));
                 console.log("Not a prime " + i + " mod " + primeLow + " is " + (i % primeLow));
                 notPrime = true;
                 break;
-              } 
-            
+              }
+
             }
               if (!notPrime) {
                 console.log("Pushing onto array " + i);
                 primeNums.push(i);
               }
-                notPrime = false;           
-            
+
+
         }
       }
   }
- 
+
     console.log(primeNums);
     var total = primeNums.reduce(reducer,0);
     console.log("The total for " + num + " is " + total);
     return total;
-  
-}
 
-sumPrimes(977);
+}
+var answerBox = document.getElementById("answer");
+
+answerBox.textContent = sumPrimes(977);
