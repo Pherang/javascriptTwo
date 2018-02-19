60236a251e02a1c3d59501df50694bf442b80601
unknown <Pherang@users.noreply.github.com>
2018-02-19 17:56:41 -0500

    Added function to find the greatest common denominator

--- smallestCommonMultiple.js
+++ smallestCommonMultiple.js
@@ -1,23 +1,23 @@
 
 function smallestCommons(arr) {
-  
+
   /* Need to find smallest common multiple between two numbers AND
   all the numbers in between
   */
-    
-  /* Need to find all the numbers in between arr[0] and arr[1].
-     They also may not be in sequential order arr[0] > arr[1] is 
-     possible. 
+
+	/* Need to find all the numbers in between arr[0] and arr[1].
+	They also may not be in sequential order arr[0] > arr[1] is
+     possible.
   */
-  
+
   /* Reading up on math, could use  the lcm (a,b) = a * b / gcd ( a,b) formula find it.
     Will also need the gcd formula which is gcd(a,b) = gcd(a-b,b)
   */
-  
+
   /* The lcm formula can be used recursively to find the lcm of multiple numbers.
       lcm (a,b,c) = lcm (a, lcm (b,c)) */
- 
-  /* Pseudo code for a recursive LCM function. 
+
+  /* Pseudo code for a recursive LCM function.
   lcm (arguments) {
 
    if (arguments == 2) {
@@ -25,20 +25,60 @@ function smallestCommons(arr) {
      b = arguments[1]
      a * b / gcd(a,b)
    return the result;
-else { 
+else {
 
            get the last two arguments and lcm them
            and also send the rest of the arguments to the next function call.
            lcm(arguments minus the last two, lcm(arguments(last), arugments(secondlast))
-     
+
        }
 
  } */
-  
-  
-  
+
+function gcd(a,b) {
+
+    var temp;
+
+    console.log(a + " vs " + b);
+
+    if (a === b) {
+
+      console.log("returned " + parseInt(a));
+
+      return a;
+
+    } else if (a > b) {
+
+        return gcd(a-b,b);
+
+    } else if (a < b) {
+
+        return gcd(a,b-a);
+
+    }
+
+    else return null;
+
+  }
+
+
+
+  var x = 0;
+
+  x = gcd(396,120);
+
+  console.log(x);
+
+  return x;
+
+
+
    return arr;
 }
 
 
-smallestCommons([1,5]);
+
+
+var answerBox = document.getElementById("answer");
+
+answerBox.textContent = smallestCommons([1,5]);
