e7f34a0a1fdeb7f990046412afa68832ffcf1f76
unknown <Pherang@users.noreply.github.com>
2018-02-19 18:37:24 -0500

    Further work on lcm function done. trying to get it to work recursively

--- smallestCommonMultiple.js
+++ smallestCommonMultiple.js
@@ -14,66 +14,52 @@ function smallestCommons(arr) {
     Will also need the gcd formula which is gcd(a,b) = gcd(a-b,b)
   */
 
-  /* The lcm formula can be used recursively to find the lcm of multiple numbers.
-      lcm (a,b,c) = lcm (a, lcm (b,c)) */
+  	/* The lcm formula can be used recursively to find the lcm of multiple numbers.
+    lcm (a,b,c) = lcm (a, lcm (b,c)) */
+		// Lowest Common Multiple takes an array of numbers
+		// Checks to make sure there are only two numbers
+		//
+		function lcm(numbers) {
+					var a;
+					var b;
+					var remainNumbers;
+					// Base Case.
 
-  /* Pseudo code for a recursive LCM function.
-  lcm (arguments) {
+			    if (numbers.length == 2{
 
-   if (arguments == 2) {
-     a = arguments[0]
-     b = arguments[1]
-     a * b / gcd(a,b)
-   return the result;
-else {
+						return numbers[0] * numbers[1] / gcd(numbers[0],numbers[1]);
 
-           get the last two arguments and lcm them
-           and also send the rest of the arguments to the next function call.
-           lcm(arguments minus the last two, lcm(arguments(last), arugments(secondlast))
+			    } else if (numbers.length > 2) {
+							// Need to split the array up or slice it.
+							a = numbers[numbers.length-1];
+							b = numbers[numbers.length-2];
+							return lcm(reducedarray, lcm(a,b));
+			    }
 
-       }
+			    else return null;
 
- } */
+			  }
 
-function gcd(a,b) {
 
-    var temp;
+				function gcd(a,b) {
 
     console.log(a + " vs " + b);
-
     if (a === b) {
-
-      console.log("returned " + parseInt(a));
-
+      //Return the Greatest Common Denominator.
       return a;
-
     } else if (a > b) {
-
         return gcd(a-b,b);
-
     } else if (a < b) {
-
         return gcd(a,b-a);
-
     }
-
     else return null;
-
   }
 
 
-
-  var x = 0;
-
-  x = gcd(396,120);
-
-  console.log(x);
-
-  return x;
-
-
-
-   return arr;
+  	var x = gcd(500,120);
+  	console.log(x);
+  	return x;
+   	return arr;
 }
 
 
