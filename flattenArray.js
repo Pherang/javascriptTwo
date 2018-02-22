c9894ad8ca05f5c6ff1475bd48695dfee8dd159f
Pherang <Pherang@users.noreply.github.com>
2018-02-21 19:37:39 -0500

    Update flattenArray.js
    function traverses an array now. Added additional comments for clarity.

--- flattenArray.js
+++ flattenArray.js
@@ -1,15 +1,16 @@
-function flatten(arr) {
-
-	var theResult = [];
+function steamrollArray(arr) {
+  	var theResult = [];
 	function traverse(element){
-
+		
+		// If element isn't an array then add it to the flattened array/
 		if (!Array.isArray(element)) {
 			console.log("The element is " + element);
 
 			theResult.push(element);
 			console.log("The result is " + theResult);
 		} else {
-			console.log("The element is not an array " + element);
+			// If the element is an array, then we check each if IT'S elements with traverse().
+			console.log("The element is actually an array " + element);
 			element.forEach(traverse);
 		}
 
