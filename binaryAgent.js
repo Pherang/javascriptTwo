69471f83c94eda0d0c71486b36afcd6ed3e36a7e
pherang <pherang@users.noreply.github.com>
2018-02-22 04:59:14 -0500

    Additional code added to findChar().

--- binaryAgent.js
+++ binaryAgent.js
@@ -6,22 +6,40 @@ function binaryAgent(str) {
    */
   /* I could create an array that had all the decimal values for each binary position */
   var binaryMap = [128,64,32,16,8,4,2,1];
+  // Split string of binary numbers into an array.
   var bins = str.split(" ");
   var words = [];
 
   // Create a function to check each binary number and convert to character.
+  // If a digit in binary is a 1 it should map to the corresponding decimal number
+  // 1000000 = 128
+  function findCode(element) {
+
+
+  }
   function findChar(element){
     var converted = [];
     converted = element.split("");
+    var charCode = converted.reduce(function(a,b,index) {
+       if (b == 1) {
+         //console.log("The answer is " acc + Number.parseInt(binaryMap[index]));
+         var z = Number.parseInt(binaryMap[index],10);
+         console.log("Z is " + z);
+         console.log(a + z);
+         return (a + z);
 
-    converted
+       }
+
+    });
+    console.log("Character code is " + charCode);
 
   }
 
-  bins.forEach(findChar(element));
+  // Full string of binary numbers has been split into elements in an array.
+  bins.forEach(findChar);
 
 
-  return str;
+  return bins;
 }
 
 
