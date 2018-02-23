59faa50457309d989340a2536aef5f332fe4b6f1
unknown <Pherang@users.noreply.github.com>
2018-02-23 07:39:22 -0500

    Added real code but not finished.

--- romanNums.js
+++ romanNums.js
@@ -5,7 +5,7 @@ function convertToRoman(num) {
     * one it is added. If it appears before a larger one it is subtracted
     * e.g.    IV is four and VI is six. Before and after.
     */
-
+    
     /* 
     * Some pseudo code for 3999 in Roman Numerals:   
     * If 3999 > 1000 then subtract 1000 and record an M.
@@ -16,13 +16,52 @@ function convertToRoman(num) {
     * If 9 > 1 then subtract 1 and record an I, and so on.
     *
     */
+    var romanThousands;
+    var romanHundreds;
+    var romanTens;
+    var romanOnes;
+
+    var thousands = 0;
+    var hundreds = 0;
+    var tens = 0;
+    var ones = 0;
+    while (num > 0) {
+        if (num >= 1000) {
+            num -= 1000;
+            thousands++;
+        }
+        if (num < 1000 && num >= 100) {
+            num -= 100;
+            hundreds++;
+        }
+        if (num < 101 && num >= 10) {
+            num -= 10;
+            tens++;
+        }
+        if (num < 11 && num >= 1) {
+            num -= 1;
+            ones++;
+        }
+
+    }
+
+    console.log(thousands + " " + hundreds + " " + tens + " " + ones);
 
     /*  
     * After we have all ones, tens, hundreds, and thousands recorded.
     * We can use a table to look up the numbers and translate them?
     * Is there an algorithm that could be programmed? We will need to know the values..
-    * 3 Ms = MMM, 900 = CM, 90 = XC, 9 = IX   
+    * 3 Ms = MMM, 900 = CM, 90 = XC, 9 = IX    
     */
+
+    if (thousands > 0 && thousands < 4) {
+        romanThousands = "MMM";
+    }
+    if (thousands = 4) {
+        romanThousands = "MV";
+    }
+
+
 
     // Finally return the calculated roman numeral.
 
@@ -33,4 +72,4 @@ function convertToRoman(num) {
 
 var answerBox = document.getElementById("answer");
 
-answerBox.textContent = convertToRoman(36);
+answerBox.textContent = convertToRoman(3996);
