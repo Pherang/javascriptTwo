6de11c0eee42f2158486e542f46a7d1e482f2938
unknown <Pherang@users.noreply.github.com>
2018-02-27 07:18:54 -0500

    Formatting and additional commenting.

--- addTogether.js
+++ addTogether.js
@@ -1,45 +1,44 @@
 function addTogether() {
 
-/* If this function receives two arguments then add them together.
-If it receives one argument then return a function that expects
-one argument and if received adds it to the previous.
+/* 
+* If this function receives two arguments then add them together.
+* If it receives one argument then return a function that expects
+* one argument and if received adds it to the previous.
+*
 */
   var answer;
   if (arguments.length == 2 ){
-      console.log("Made it in");
-      if (!Number.isInteger(arguments[0]) || !Number.isInteger(arguments[1])) {
-        console.log("Array is not a number");
+    console.log("Made it in");
+    if (!Number.isInteger(arguments[0]) || !Number.isInteger(arguments[1])) {
+      console.log("Array is not a number");
+      return undefined;
+    } else {
+      return Number.parseInt(arguments[0] + arguments[1]);
+    }
+  } 
+  if (arguments.length == 1) {
+    var oldnum = arguments[0];
+
+    if (!Number.isInteger(oldnum)) {
+      return undefined;
+    } else
+    /* 
+    * return a function that expects a number.
+    * Using the idea of closure here. The function add() is a closure that expects another number which it adds to its oldnum value and returns the total.
+    */
+    return (function add(newnum) {
+      if (!Number.isInteger(newnum)){
         return undefined;
-      } else {
-        return Number.parseInt(arguments[0] + arguments[1]);
-      }
-    } else if (arguments.length == 1) {
-      var oldnum = arguments[0];
+      } else
+      return oldnum + newnum;
+    });
+  } else {
 
-        if (!Number.isInteger(oldnum)) {
-          return undefined;
-        } else
-        /* return a function that expects a number.
-           Using the idea of closure here. The function add() is a closure that contains
-
-        */
-        return (function add(newnum) {
-          if (!Number.isInteger(newnum)){
-            return undefined;
-          } else
-          return oldnum + newnum;
-        });
-      } else {
-
-        return undefined;
-      }
+    return undefined;
+  }
 } // End addTogether
 
 var answerBox = document.getElementById("answer");
 x = addTogether(2, "3");
-
-if (x === undefined) {
-  x = "undefined";
-}
 
 answerBox.textContent = x;
