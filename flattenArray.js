ea88578fa18247859d760191b136b56784822895
unknown <Pherang@users.noreply.github.com>
2018-03-07 07:02:49 -0500

    Cleaned up comments

--- flattenArray.js
+++ flattenArray.js
@@ -4,13 +4,9 @@ function steamrollArray(arr) {
 		
 		// If element isn't an array then add it to the flattened array/
 		if (!Array.isArray(element)) {
-			console.log("The element is " + element);
-
 			theResult.push(element);
-			console.log("The result is " + theResult);
 		} else {
 			// If the element is an array, then we check each if IT'S elements with traverse().
-			console.log("The element is actually an array " + element);
 			element.forEach(traverse);
 		}
 
